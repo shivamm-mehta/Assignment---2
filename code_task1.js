446e1da0e200de7189932be821203551835fb69c
shivamm-mehta <mehtas25@mymacewan.ca>
2024-02-08 17:28:44 -0700

    Assignment Summited

---
+++ code_task1.js
@@ -0,0 +1,9 @@
+//Coding Task 1 
+//On a single line, declare three variables with the given names and values: firstName = "Moe" lastName = "Adam" age = 52
+
+let firstName = "Moe"; 
+let lastName = "Adam"; 
+let age = 52;
+
+
+console.log("First name : ",firstName, "Last name : ",lastName, "Age : ",age);
